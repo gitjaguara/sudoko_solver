a39d780278a76d6bf8e958f7793e7c9a033f9b7e
gitjaguara <63111866+gitjaguara@users.noreply.github.com>
2022-08-01 22:49:10 +0530

    collecting 2 only guesses

--- sudoko.py
+++ sudoko.py
@@ -2,7 +2,8 @@ from pprint import pprint
 
 rows, cols = (9, 9)
 sud_mtx = [[0] * cols] * rows
-gm = [
+#easy
+ez = [
     [0, 7, 0, 0, 2, 0, 0, 4, 6],
     [0, 6, 0, 0, 0, 0, 8, 9, 0],
     [2, 0, 0, 8, 0, 0, 7, 1, 5],
@@ -13,6 +14,19 @@ gm = [
     [0, 5, 8, 0, 0, 0, 0, 6, 0],
     [4, 3, 0, 0, 8, 0, 0, 7, 0]
 ]
+#hard
+hd = [
+    [0, 0, 0, 0, 0, 7, 0, 0, 6],
+    [0, 9, 4, 0, 3, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 2, 0, 0],
+    [1, 0, 0, 0, 8, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 2, 8, 3, 1],
+    [7, 0, 0, 0, 1, 0, 0, 4, 9],
+    [0, 0, 0, 0, 0, 0, 0, 0, 5],
+    [5, 0, 1, 9, 0, 0, 0, 6, 3],
+    [0, 7, 0, 0, 6, 0, 9, 2, 8]
+]
+
 
 sz = 3
 static_set = set(range(1, sz * sz + 1))
@@ -29,7 +43,7 @@ def get_row_set(mtx, r, c):
 
 def get_col_set(mtx, r, c):
     cr = mtx[r][c]
-    cs = set([i[c] for i in mtx]) - {cr} - {0} # TODO not right way of column pick
+    cs = set([i[c] for i in mtx]) - {cr} - {0}
     # print("cl")
     # pprint(cs)
     return cs
@@ -49,17 +63,22 @@ def get_squ_set(mtx, r, c):
     cs = x - {cr} - {0}
     return cs
 
+gm=hd
+# gm=ez
 
 pprint(gm)
 print("")
 # print(static_set)
 # print(sud_mtx)
+
 mc = 0
 added = True
+solved = 0
 while added:
     added = False
     mc = mc + 1
-    print("Cycle {0}".format(mc))
+    twicy = {}
+    print("Cycle {0} solved {1}".format(mc,solved))
     for i in range(0, l):
         for j in range(0, l):
             if not gm[i][j]:
@@ -67,8 +86,13 @@ while added:
                 colset = get_col_set(gm, i, j)
                 squset = get_squ_set(gm, i, j)
                 res = static_set - (rowset | colset | squset)
-                if len(res) == 1:
+                op = len(res)
+                if op == 1:
                     gm[i][j] = list(res)[0]
                     added = True
+                    solved=solved+1
+                if op == 2:
+                    twicy[(i,j)] = res
+    pprint(twicy)
     # pprint(gm)
 pprint(gm)
